4b643becfd1d551bb7eb36e8e0f54d1f1ce669e1
RikuVesanto <riku.vesanto@gmail.com>
2022-01-10 18:58:11 +0200

    Add routing for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import ExercisesView from "./ExercisesView";
+import AdminView from "./AdminView";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div className="topnav">
+          <div id="myLinks">
+            <Link className="link" to="/exercises">
+              Exercises
+            </Link>
+            <Link className="link" to="/admin">
+              Admin
+            </Link>
+          </div>
+        </div>
+        <Routes>
+          <Route path="/exercises" element={<ExercisesView />} />
+          <Route path="/admin" element={<AdminView />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
