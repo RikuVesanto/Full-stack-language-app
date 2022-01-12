fc8b884728c5e091d261ae4a5fd3f8ee540f31a5
RikuVesanto <riku.vesanto@gmail.com>
2022-01-12 13:20:16 +0200

    Fetches Finnish words as well now.

--- frontend/src/ExercisesView.js
+++ frontend/src/ExercisesView.js
@@ -3,6 +3,7 @@ import Table from "./Table";
 
 function ExerciseView(props) {
   const [englishWords, setEnglishWords] = useState([]);
+  const [finnishWords, setFinnishWords] = useState([]);
 
   useEffect(() => {
     loadData().then((data) => loopData(data));
@@ -16,14 +17,19 @@ function ExerciseView(props) {
 
   function loopData(data) {
     var tempEnglishWords;
+    var tempFinnishWords;
     for (var i = 0; i <= data.length; i++) {
       if (i === 0) {
         tempEnglishWords = [].concat(data[i].english);
+        tempFinnishWords = [].concat(data[i].finnish);
       } else {
         tempEnglishWords = englishWords.concat(data[i].english);
+        tempFinnishWords = finnishWords.concat(data[i].finnish);
       }
       console.log(tempEnglishWords);
+      console.log(tempFinnishWords);
       setEnglishWords(tempEnglishWords);
+      setFinnishWords(tempFinnishWords);
     }
   }
   return (
