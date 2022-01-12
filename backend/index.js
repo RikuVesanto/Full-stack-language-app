e1109fe428d43c3f29eba95a8bb9c3fa24d04bd6
RikuVesanto <riku.vesanto@gmail.com>
2022-01-12 12:52:29 +0200

    Add cors dependency

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const getWord = require("./routes/getWord.js");
 const getWords = require("./routes/getWords.js");
 const postWord = require("./routes/postWord.js");
@@ -7,6 +8,7 @@ const deleteWord = require("./routes/deleteWord.js");
 const modifyWord = require("./routes/modifyWord.js");
 const connection = require("./database.js");
 
+app.use(cors());
 app.use(express.json());
 app.use("/words", postWord);
 app.use("/words", getWords);
